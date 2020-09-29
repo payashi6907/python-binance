c36673c9c848bce069f2ba6916b0f7672110f391
payashi6907 <takumi@TakuminoMacBook-puro.local>
2020-09-29 16:13:38 +0900

    swap add

--- binance/client.py
+++ binance/client.py
@@ -3702,3 +3702,37 @@ class Client(object):
 
         """
         return self._request_futures_api('get', 'income', True, data=params)
+
+    def get_all_asset_balance(self, **params):
+        #added by Takumi
+        res = self.get_account(**params)
+        all_balances = []
+        if "balances" in res:
+            for bal in res['balances']:
+                if float(bal["free"]) > 0  or float(bal["locked"]) > 0:
+                    all_balances.append(bal)
+        return all_balances
+    
+    def get_all_pools(self, **params):
+        #added by Takumi
+        return self._request_margin_api('get', 'bswap/pools', True, data=params)
+
+    def get_pool_liquidity(self, **params):
+        #added by Takumi
+        #params = poolId
+        return self._request_margin_api('get', 'bswap/liquidity', True, data=params)
+
+    def get_swap_price(self, **params):
+        #added by Takumi
+        #params = quoteAsset, baseAsset, quoteQty < 5000
+        return self._request_margin_api('get', 'bswap/quote', True, data=params)
+
+    def add_liquidity(self,**params):
+        #added by Takumi
+        #params = poolId, asset, quantity
+        return self._request_margin_api('get', 'bswap/liquidityAdd', True, data=params)
+    
+    def remove_liquidity(self,**params):
+        #added by Takumi
+        #params = poolId, type = "SINGLE" or "COMBINATION", asset =["coinname1","coinname2"],shareAmount < half of amount
+        return self._request_margin_api('get', 'bswap/liquidityRemove', True, data=params)
